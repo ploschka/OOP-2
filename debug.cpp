e5d5e8e5e51c7e970ad98515b5e8236335bd3045
Ploshka <folga12381@gmail.com>
2022-05-03 19:58:52 +1000

    Preparations for another modules

--- debug.cpp
+++ debug.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include "KVC.h"
 #include "Stack.h"
+#include "BigInteger.h"
 
 int main()
 {
     KVC<char, int> mapa;
-    Stack<char> staka;
+    mapa['a'] = 5;
+    std::cout << mapa['a'] << std::endl;
+    mapa['a'] = 6;
+    mapa['b'] = 7;
+    std::cout << mapa['b'] << std::endl;
+    std::cout << mapa['a'] << std::endl;
+
+    KVC<char, int> mapa2(mapa);
+
+    std::cout << "===================" << std::endl;
+
+    std::cout << mapa2['a'] << std::endl << mapa2['b'] << std::endl;
+    std::cout << "===================================\n" << "===================================\n";
+
+    Stack<int> staka;
+    staka.push(5);
+    staka.push(3);
+    staka.push(2);
+    std::cout << staka << std::endl;
+    std::cout << staka.pop() << std::endl;
+    std::cout << staka << std::endl;
+    std::cout << "===================================\n" << "===================================\n";
+
+    BigInteger biga("899");
+    BigInteger bigb("1");
+    std::cout << (biga + bigb) << std::endl;
+    //6912
 }
